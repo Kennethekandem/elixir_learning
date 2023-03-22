0b5ef5876762bdc0ed06e49a84ce21929613ff0a
Kenneth Ekandem <kennyekandem@gmail.com>
2023-03-22 12:07:19 +0100

    signin

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -14,6 +14,21 @@ defmodule DiscussWeb.AuthController do
     end
   end
 
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(user_id: user.id)
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in!")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+    end
+  end
+
   defp insert_or_update_user(changeset) do
 
     case Repo.get_by(User, :email, changeset.changes.email) do
