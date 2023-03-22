d71d0ed3f4ef648b29e280df5cddcbef367ff8bb
Kenneth Ekandem <kennyekandem@gmail.com>
2023-03-22 09:54:22 +0100

    insert or update user

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,9 +2,26 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.inspect(conn.assigns, label: "assigns: ")
-    # IO.inspect(params, label: "params: ")
+  alias Discuss.{User, Repo}
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+
+    changeset = User.changeset(%User{}, user_params)
+
+    with {:ok, user} <- insert_or_update_user(changeset) do
+
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+
+    case Repo.get_by(User, :email, changeset.changes.email) do
+      user ->
+        {:ok, user}
+      nil ->
+        Repo.insert(changeset)
+    end
   end
 
 end
