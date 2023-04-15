90edb4dab283a46204d04ec46913a5e476f08a85
Kenneth Ekandem <kennyekandem@gmail.com>
2023-04-15 08:51:55 +0100

    use plug to keep track of user_id

--- discuss/lib/discuss_web/plugs/set_user.ex
+++ discuss/lib/discuss_web/plugs/set_user.ex
@@ -1,4 +1,20 @@
 defmodule DiscussWeb.Plugs.SetUser do
   import Plug.Conn
   import Phoenix.Controller
+
+  alias Discuss.Repo
+  alias Discuss.User
+
+  def init(default), do: default
+
+  def call(conn, _default) do
+    user_id =
+    conn
+    |> get_session(:user_id)
+
+    cond do
+       ->
+        
+    end
+  end
 end
