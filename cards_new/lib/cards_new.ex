00702ff9710c5bb8efe02a394c783118859e6e09
Kenneth Ekandem <kennyekandem@gmail.com>
2024-01-17 21:29:51 +0100

    starting elixir again

---
+++ cards_new/lib/cards_new.ex
@@ -0,0 +1,18 @@
+defmodule CardsNew do
+  @moduledoc """
+  Documentation for `CardsNew`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CardsNew.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ cards_new/test/cards_new_test.exs
@@ -0,0 +1,8 @@
+defmodule CardsNewTest do
+  use ExUnit.Case
+  doctest CardsNew
+
+  test "greets the world" do
+    assert CardsNew.hello() == :world
+  end
+end
